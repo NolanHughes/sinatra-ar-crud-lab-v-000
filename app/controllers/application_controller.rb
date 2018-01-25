aa05a6412ddba963ff450ad11ed9b505e945955d
Nolan Hughes <nhughes987@gmail.com>
2018-01-25 21:56:35 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,33 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/' do
+    "First page"
+  end
+
+  get '/posts/new' do
+    erb :'new'
+  end
+
+  post '/posts' do
+    @post = Post.create(name: params[:name], content: params[:content])
+
+    erb :'show'
+  end
+
+  get '/posts' do
+    @posts = Post.all
+
+    erb :'index'
+  end
+
+  get '/posts/:id' do
+    @post = Post.find_by(params[:id])
+
+    erb :'show'
+  end
+
+  get '/posts/:id/edit' do
+    erb :'edit'
   end
 end
