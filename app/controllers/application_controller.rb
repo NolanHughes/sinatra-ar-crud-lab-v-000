e12310f24096fa7812c7014aff4af243e999b563
Nolan Hughes <nhughes987@gmail.com>
2018-01-25 22:44:51 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,7 +34,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/posts/:id/edit' do
-    @post = Post.find_by(params[:id])
+    @post = Post.find(params[:id])
 
     erb :'edit'
   end
